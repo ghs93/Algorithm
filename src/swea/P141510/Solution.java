1684be85cc4be25563286d9e009fb569d1a62929
간호성 <ghs93@kakao.com>
2022-10-23 23:40:03 +0900

    SWEA 14150. 나무 높이

--- src/swea/P141510/Solution.java
+++ src/swea/P141510/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
 			
 			st = new StringTokenizer(br.readLine());
 			
-			int max = 0;
+			int max = 0; //가장 큰 나무의 높이
 			int[] trees = new int[N];
 			for (int i = 0; i < N; i++) {
 				int t = Integer.parseInt(st.nextToken());
@@ -31,6 +31,7 @@ public class Solution {
 				max = Math.max(max, t);
 			}
 			
+			//나무가 자라야 할 높이에서 필요한 1, 2의 개 수 구하기
 			int even = 0, odd = 0;
 			for (int i = 0; i < N; i++) {
 				int diff = max - trees[i];
@@ -40,8 +41,8 @@ public class Solution {
 				even += diff / 2;
 				odd += diff % 2;
 			}
-
-			int result = 0;
+			
+			//2 -> 1로 변경
 			if(even > odd) {
 				while(Math.abs(even - odd) > 1) {
 					even--;
@@ -49,13 +50,14 @@ public class Solution {
 				}
 			}
 			
-			if(odd > even) {
+			int result = 0;
+			if(odd > even) { //1이 많을 경우
 				result = odd * 2 - 1;
 				
-			} else if(even > odd) {
-				result = (odd * 2) + (even - odd) * 2;
+			} else if(even > odd) { //2가 많을 경우 
+				result = even * 2;
 				
-			} else {
+			} else { //1과 2의 숫자가 같을 경우
 				result = odd + even;
 			}
 			
