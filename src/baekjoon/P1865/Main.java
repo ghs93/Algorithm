a7a0ebae8f7ec750337fc439ade6cb7255abc6f6
간호성 <ghs93@kakao.com>
2022-10-31 23:28:44 +0900

    백준 1865. 웜홀

--- src/baekjoon/P1865/Main.java
+++ src/baekjoon/P1865/Main.java
@@ -28,24 +28,53 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 0; tc < T; tc++) {
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < TC; tc++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int N = Integer.parseInt(st.nextToken()); //지점 수
 			int M = Integer.parseInt(st.nextToken()); //도로 수
 			int W = Integer.parseInt(st.nextToken()); //웜홀 수
 			
-			long[] path = new long[N+1];
-			Arrays.fill(path, Long.MAX_VALUE);
-			path[1] = 0;
-			
 			List<Edge> edges = new ArrayList<>();
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int S = Integer.parseInt(st.nextToken());
+				int E = Integer.parseInt(st.nextToken());
+				int T = Integer.parseInt(st.nextToken());
+				
+				edges.add(new Edge(S, E, T));
+				edges.add(new Edge(E, S, T));
 			}
+			
+			for (int i = 0; i < W; i++) {
+				st = new StringTokenizer(br.readLine());
+				int S = Integer.parseInt(st.nextToken());
+				int E = Integer.parseInt(st.nextToken());
+				int T = Integer.parseInt(st.nextToken());
+				
+				edges.add(new Edge(S, E, T * -1));
+			}
+			
+			long[] path = new long[N+1];
+			boolean isPossible = false;
+			Arrays.fill(path, Integer.MAX_VALUE);
+			path[1] = 0;
+
+			for (int i = 1; i <= N-1; i++) {
+				for (Edge edge : edges) {
+					path[edge.to] = Math.min(path[edge.to], path[edge.from] + edge.weight);
+				}
+			}
+			
+			for (Edge edge : edges) {
+				if(path[edge.to] > path[edge.from] + edge.weight) {
+					isPossible = true;
+					break;
+				}
+			}
+
+			System.out.println(isPossible ? "YES" : "NO");
 		}
 	}
-
 }
