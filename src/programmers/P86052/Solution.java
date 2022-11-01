a628ee1124c1c2f185c621fc416425d270975929
ghs93 <ghs93@kakao.com>
2022-11-01 21:57:00 +0900

    Programmers 86052. 빛의 경로 사이클

---
+++ src/programmers/P86052/Solution.java
@@ -0,0 +1,26 @@
+package programmers.P86052;
+
+import java.util.Arrays;
+
+/**
+ * Programmers 86052. 빛의 경로 사이클 
+ * @author hoseong 
+ * @category
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+		int[] result = solution(new String[] { "SL", "LR" });
+		//int[] result = solution(new String[] {"S"});
+		// int[] result = solution(new String[] {"R", "R"});
+
+		System.out.println(Arrays.toString(result));
+	}
+
+	//모든 정점에서 출발해봐야 함
+	public static int[] solution(String[] grid) {
+		int[] answer = {};
+		
+		return answer;
+	}
+}
