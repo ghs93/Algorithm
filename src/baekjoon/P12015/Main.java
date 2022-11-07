83d58d8a5b218b0561cb9c427d3eda6f78029365
간호성 <ghs93@kakao.com>
2022-11-08 00:32:28 +0900

    백준 12015. 가장 긴 증가하는 부분 수열 2

--- src/baekjoon/P12015/Main.java
+++ src/baekjoon/P12015/Main.java
@@ -1,41 +1,42 @@
-package baekjoon.P11053;
+package baekjoon.P12015;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * 백준 11053. 가장 긴 증가하는 부분 수열 - 실버 2
- * 일반 DP 사용
+ * 백준 12015. 가장 긴 증가하는 부분 수열 2 - 골드 2
+ *  DP 이진 검색 사용
  * @author hoseong
  * @category DP
  */
-public class Main_DP1 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		int[] dp = new int[N];
+		int[] LIS = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int max = 0;
+		int size = 0;
 		for (int i = 0; i < N; i++) {
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if(arr[i] > arr[j] && dp[i] < dp[j]+1)
-					dp[i] = dp[j] + 1;				
-			}
+			int pos = Arrays.binarySearch(LIS, 0, size, arr[i]);
+			if(pos >= 0) continue;
 			
-			max = Math.max(max, dp[i]);
+			int iPos = Math.abs(pos) - 1;
+			LIS[iPos] = arr[i];
+			
+			if(iPos == size) size++;
 		}
 		
-		System.out.println(max);
+		System.out.println(size);
 	}
 }
