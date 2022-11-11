193edb5c3f471486296ce9473777fc664032cf6a
간호성 <ghs93@kakao.com>
2022-11-11 17:57:35 +0900

    Programmers 133500. 등대

--- src/programmers/P133500/Solution.java
+++ src/programmers/P133500/Solution.java
@@ -1,145 +1,13 @@
 package programmers.P133500;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Queue;
 
 /**
  * Programmers 133500. 등대 
  * @author hoseong 
- * @category 
+ * @category DP
  */
 public class Solution {
-
-	public static void main(String[] args) {
-		int result1 = solution(8, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}});
-		int result2 = solution(10, new int[][] {{4, 1}, {5, 1}, {5, 6}, {7, 	6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}});
-		int result3 = solution(5, new int[][] {{1, 2}, {2, 3}, {3, 4}, {4, 5}});
-		int result4 = solution(13, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 6}, {4, 7}, {5, 8}, {3, 9}, {9, 10}, {9, 11}, {9, 12}, {11, 13}});
-		int result5 = solution(2, new int[][] {{1,2}});
-		int result6 = solution(10, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {6, 7}, {6, 8}, {6, 9}, {5, 10}});
-		
-		System.out.println(result1);
-		System.out.println(result2);
-		System.out.println(result3);
-		System.out.println(result4);
-		System.out.println(result5);
-		System.out.println(result6);
-	}
-
-	public static int solution(int n, int[][] lighthouse) {
-        Node[] nodes = new Node[n+1];
-        for (int i = 0; i <= n; i++) {
-			nodes[i] = new Node(i, 0);
-		}
-        
-        //루트노드 찾기(연결된 간선이 가장 많은 노드)
-        int root = 0, maxConn = 0;
-        for (int i = 0; i < n-1; i++) {
-			int from = lighthouse[i][0];
-			int to = lighthouse[i][1];
-			
-			nodes[from].add(to);
-			nodes[to].add(from);
-			
-			if(maxConn < nodes[from].connection) {
-				root = from;
-				maxConn = nodes[from].connection;
-			}
-			
-			if(maxConn < nodes[to].connection) {
-				root = to;
-				maxConn = nodes[to].connection;
-			}
-		}
-        
-        Queue<Node> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        
-        //루트의 불을 켰을 때
-        int onRoot = 0;
-        q.add(nodes[root]);
-        while(!q.isEmpty()) {
-        	Node node = q.poll();
-        	visited[node.from] = true;
-        	
-        	//불 켜기
-        	onRoot++;
-        	
-        	//자식의 자식 추가
-        	for (int to : node.to) {
-				//자식 노드가 리프노드면 건너뛰기
-        		if(nodes[to].connection < 2 || visited[to]) continue;
-        		
-        		visited[to] = true;
-        		boolean check = true;
-        		for (int t : nodes[to].to) {
-        			if(visited[t]) continue;
-        			
-        			//자식 모두가 리프노드 인가 검사
-					if(nodes[t].connection > 1)
-						check = false;
-					
-					q.add(nodes[t]);
-				}
-        		
-        		//자식노드의 자식들이 모두 리프노드일 경우
-        		if(check) {
-        			for (int t : nodes[to].to) {
-    					q.remove(nodes[t]);
-    				}
-        			
-        			q.add(nodes[to]);
-        		}
-			}
-        }
-        
-        Arrays.fill(visited, false);
-        
-        //루트노드의 불을 껐을 때
-        int offRoot = 0;
-        visited[root] = true;
-        for(int to : nodes[root].to)
-        	q.add(nodes[to]);
-        
-        while(!q.isEmpty()) {
-        	Node node = q.poll();
-        	visited[node.from] = true;
-        	
-        	offRoot++;
-        	
-        	//자식의 자식 추가
-        	for (int to : node.to) {
-				//자식 노드가 리프노드면 건너뛰기
-        		if(nodes[to].connection < 2 || visited[to]) continue;
-        		
-        		visited[to] = true;
-        		boolean check = true;
-        		for (int t : nodes[to].to) {
-        			if(visited[t]) continue;
-        			
-        			//자식 모두가 리프노드 인가 검사
-					if(nodes[t].connection > 1)
-						check = false;
-					
-					q.add(nodes[t]);
-				}
-        		
-        		//자식노드의 자식들이 모두 리프노드일 경우
-        		if(check) {
-        			for (int t : nodes[to].to) {
-    					q.remove(nodes[t]);
-    				}
-        			
-        			q.add(nodes[to]);
-        		}
-			}
-        }
-        System.out.println("on: " + onRoot + ", off: " + offRoot);
-        return Math.min(onRoot, offRoot);
-    }
-	
 	static class Node {
 		ArrayList<Integer> to;
 		int from, connection;
@@ -159,5 +27,64 @@ public class Solution {
 		public String toString() {
 			return "Node [to=" + to + ", from=" + from + ", connection=" + connection + "]";
 		}
+	}
+
+	public static void main(String[] args) {
+		int result1 = solution(8, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}});
+		int result2 = solution(10, new int[][] {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}});
+		int result3 = solution(5, new int[][] {{1, 2}, {2, 3}, {3, 4}, {4, 5}});
+		int result4 = solution(13, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 6}, {4, 7}, {5, 8}, {3, 9}, {9, 10}, {9, 11}, {9, 12}, {11, 13}});
+		int result5 = solution(2, new int[][] {{1,2}});
+		int result6 = solution(10, new int[][] {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {6, 7}, {6, 8}, {6, 9}, {5, 10}});
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+		System.out.println(result6);
+	}
+
+	static Node[] nodes;
+	public static int solution(int n, int[][] lighthouse) {
+		if(n == 2)
+			return 1;
+		
+        nodes = new Node[n+1];
+        for (int i = 0; i <= n; i++) {
+			nodes[i] = new Node(i, 0);
+		}
+        
+        for (int i = 0; i < n-1; i++) {
+			int from = lighthouse[i][0];
+			int to = lighthouse[i][1];
+			
+			nodes[from].add(to);
+			nodes[to].add(from);
+		}
+        
+        boolean[] visited = new boolean[n+1];
+        int[][] dp = new int[2][n+1];
+        
+        getDp(nodes[1], visited, dp);
+        
+        return Math.min(dp[0][1], dp[1][1]);
+    }
+	
+	public static void getDp(Node node, boolean[] visited, int[][] dp) {
+		visited[node.from] = true;
+		
+		int on = 1, off = 0;
+		for (int to : node.to) {
+			if(visited[to]) continue;
+			
+			getDp(nodes[to], visited, dp);
+			
+			on += Math.min(dp[0][to], dp[1][to]);
+			off += dp[0][to];
+		}
+		
+		dp[0][node.from] = on;
+		dp[1][node.from] = off;
 	}
 }
