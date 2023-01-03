3d3e17bf38688cfca7a9f01b764b699c9c42f5d7
ghs93 <ghs93@kakao.com>
2023-01-03 21:59:41 +0900

    백준 5719. 거의 최단 경로

--- src/baekjoon/P5719/Main.java
+++ src/baekjoon/P5719/Main.java
@@ -17,12 +17,10 @@ import java.util.StringTokenizer;
 public class Main {
 	static class Node implements Comparable<Node> {
 		int to, weight;
-        boolean isVisited;
 
 		public Node(int to, int weight) {
 			this.to = to;
 			this.weight = weight;
-            this.isVisited = false;
 		}
 
 		@Override
@@ -32,7 +30,7 @@ public class Main {
 	}
 
 	static List<Node>[] nodes;
-	static Node[] nArr;
+	static List<Node>[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -47,9 +45,11 @@ public class Main {
 			if(N == 0 && M == 0) break;
 			
 			nodes = new ArrayList[N];
+			visited = new ArrayList[N];
 
 			for (int i = 0; i < N; i++) {
 				nodes[i] = new ArrayList<>();
+				visited[i] = new ArrayList<>();
 			}
 
 			st = new StringTokenizer(br.readLine());
@@ -58,77 +58,43 @@ public class Main {
 			
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
-				int U = Integer.parseInt(st.nextToken());				
-				int V = Integer.parseInt(st.nextToken());				
+				int U = Integer.parseInt(st.nextToken());
+				int V = Integer.parseInt(st.nextToken());
 				int P = Integer.parseInt(st.nextToken());
 				
 				nodes[U].add(new Node(V, P));
 			}
 			
-			nArr = new Node[N];
-			int min = dijkstra(S, D, N)[D];
-			deletePath(D);
+			int min = 0;
 			
-			int next = 0;
-			while(true) {
-				Arrays.fill(nArr, null);
-				next = dijkstra(S, D, N)[D];
-				
-				if(min != next)
-					break;
-				
-				deletePath(D);
-			}
-			
-			sb.append(next == Integer.MAX_VALUE ? -1 : next).append('\n');
+			sb.append(min != Integer.MAX_VALUE ? min : -1).append('\n');
 		}
 		
 		System.out.println(sb);
 	}
-	
-	public static int[] dijkstra(int start, int end, int N) {
+
+	private static void dijkstra(int s, int d, int n) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
+		int[] D = new int[n];
+		Arrays.fill(D, Integer.MAX_VALUE);
 		
-		int[] d = new int[N];
-		Arrays.fill(d, Integer.MAX_VALUE);
-		
-		pq.add(new Node(start, 0));
-		d[start] = 0;
+		pq.add(new Node(s, 0));
+		D[s] = 0;
 		
 		while(!pq.isEmpty()) {
-			Node n = pq.poll();
+			Node node = pq.poll();
 			
-			if(n.weight > d[n.to]) continue;
+			if(node.weight > D[node.to]) continue;
 			
-			for (Node nd : nodes[n.to]) {
-                if(nd.isVisited) continue;
-                
-				int cost = d[n.to] + nd.weight;
+			for(Node nd : nodes[node.to]) {
 				
-				if(d[nd.to] > cost) {
-					d[nd.to] = cost;
+				int cost = D[node.to] + nd.weight;
+				
+				if(D[nd.to] > cost) {
 					pq.add(new Node(nd.to, cost));
-					nArr[nd.to] = n;
+					D[nd.to] = cost;
 				}
 			}
-		}
-		
-		return d;
-	}
-	
-	static void deletePath(int end) {
-		Node e = nArr[end];
-		int from = end;
-		while(e != null) {
-			for (Node nd : nodes[e.to]) {
-				if(nd.to == from) {
-                    nd.isVisited = true;
-					break;
-				}
-			}
-			
-			from = e.to;
-			e = nArr[e.to];
 		}
 	}
 }
