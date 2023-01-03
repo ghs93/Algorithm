728543596c781cf0c291c83d7bb3d4e5d908e111
간호성 <ghs93@kakao.com>
2023-01-04 00:19:09 +0900

    백준 5719. 거의 최단 경로

--- src/baekjoon/P5719/Main.java
+++ src/baekjoon/P5719/Main.java
@@ -27,10 +27,17 @@ public class Main {
 		public int compareTo(Node o) {
 			return this.weight - o.weight;
 		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Node node = (Node) obj;
+			return this.to == node.to;
+		}
 	}
 
-	static List<Node>[] nodes;
-	static List<Node>[] visited;
+	static List<Node>[] nodes; //입력 받은 모든 도로
+	static List<Node>[] visited; //최단 경로 도로
+	static int[] minDist;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,6 +53,7 @@ public class Main {
 			
 			nodes = new ArrayList[N];
 			visited = new ArrayList[N];
+			minDist = new int[N];
 
 			for (int i = 0; i < N; i++) {
 				nodes[i] = new ArrayList<>();
@@ -65,36 +73,58 @@ public class Main {
 				nodes[U].add(new Node(V, P));
 			}
 			
-			int min = 0;
+			//최단 경로 구하기
+			dijkstra(S, D);
+		
+			//최단 경로 지우기
+			remove(D);
 			
-			sb.append(min != Integer.MAX_VALUE ? min : -1).append('\n');
+			//거의 최단 경로 구하기
+			dijkstra(S, D);
+			
+			sb.append(minDist[D] != Integer.MAX_VALUE ? minDist[D] : -1).append('\n');
 		}
 		
 		System.out.println(sb);
 	}
 
-	private static void dijkstra(int s, int d, int n) {
+	private static void dijkstra(int s, int d) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		int[] D = new int[n];
-		Arrays.fill(D, Integer.MAX_VALUE);
+		Arrays.fill(minDist, Integer.MAX_VALUE);
 		
 		pq.add(new Node(s, 0));
-		D[s] = 0;
+		minDist[s] = 0;
 		
 		while(!pq.isEmpty()) {
 			Node node = pq.poll();
 			
-			if(node.weight > D[node.to]) continue;
+			if(node.weight > minDist[node.to]) continue;
 			
 			for(Node nd : nodes[node.to]) {
+				int cost = minDist[node.to] + nd.weight;
 				
-				int cost = D[node.to] + nd.weight;
-				
-				if(D[nd.to] > cost) {
+				//기존에 구한 거리와 같다면 방문 처리만하고 넘어가기
+				if(minDist[nd.to] == cost) {
+					visited[nd.to].add(node);
+					
+				} else if(minDist[nd.to] > cost) {
+					//새로운 최단 거리가 발생하면 기존 방문기록 삭제 후 새로 기록
+					minDist[nd.to] = cost;
+					
+					visited[nd.to].clear();
+					visited[nd.to].add(node);
 					pq.add(new Node(nd.to, cost));
-					D[nd.to] = cost;
 				}
 			}
+		}
+	}
+	
+	private static void remove(int idx) {
+		for(int i=visited[idx].size()-1; i>=0; i--) {
+			Node node = visited[idx].remove(i);
+			nodes[node.to].remove(new Node(idx, 0)); //기존 경로에서 최단경로 지우기
+			
+			remove(node.to);
 		}
 	}
 }
