70cb4162d9e8a0de6fe06dcfb76d8e6a22961e50
간호성 <ghs93@kakao.com>
2023-01-29 23:10:52 +0900

    백준 1766. 문제집

---
+++ src/baekjoon/P1766/Main.java
@@ -0,0 +1,60 @@
+package baekjoon.P1766;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 1766. 문제집 - 골드 2
+ * @author hoseong
+ * @category 위상정렬
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>[] arr = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        int[] inDegree = new int[N+1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            arr[from].add(to);
+            inDegree[to]++;
+        }
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) {
+            if(inDegree[i] == 0)
+                q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            for (int i = 0, size = q.size(); i < size; i++) {
+                int parent = q.poll();
+                sb.append(parent).append(' ');
+
+                for(int ch : arr[parent]) {
+                    if(--inDegree[ch] == 0)
+                        q.add(ch);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
