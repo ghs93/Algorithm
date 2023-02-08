f970e9bdc0e0974f9650ba80dc29b7b5e1566126
간호성 <ghs93@kakao.com>
2023-02-08 22:22:25 +0900

    백준 11399. ATM

---
+++ src/baekjoon/P11399/Main.java
@@ -0,0 +1,36 @@
+package baekjoon.P11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 11399. ATM - 실버 4
+ * @author hoseong
+ * @category 정렬
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            q.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int total = 0;
+        int before = 0;
+        while (!q.isEmpty()) {
+            before += q.poll();
+            total += before;
+        }
+
+        System.out.println(total);
+    }
+}
