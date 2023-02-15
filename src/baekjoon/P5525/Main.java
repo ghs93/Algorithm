6af844da40d8bb36a20d3c3900cc2386806cf5db
간호성 <ghs93@kakao.com>
2023-02-15 22:00:05 +0900

    백준 5525. IOIOI

--- src/baekjoon/P5525/Main.java
+++ src/baekjoon/P5525/Main.java
@@ -19,48 +19,24 @@ public class Main {
         M = Integer.parseInt(br.readLine());
         String S = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= N * 2; i++) {
-            sb.append(i % 2 == 0 ? 'I' : 'O');
-        }
+        int count = 0;
+        int result = 0;
+        for (int i = 1; i < M-1; i++) {
+            if(S.charAt(i-1) == 'I' && S.charAt(i) == 'O' && S.charAt(i+1) == 'I') {
+                count++;
 
-        String ioi = sb.toString();
-        int cnt = 0;
-
-        int size = M - (2 * N);
-        for (int i = 0; i <= size; i++) {
-            if(S.charAt(i) == ioi.charAt(0) && check(i, S, ioi)) {
-                cnt++;
-
-                int tmp = i + (2 * N) + 1;
-                while (tmp + 1 < M) {
-                    if(S.charAt(tmp) == 'O' && S.charAt(tmp + 1) == 'I') {
-                        cnt++;
-                        tmp += 2;
-
-                    } else {
-                        break;
-                    }
+                if(count == N) {
+                    count--;
+                    result++;
                 }
 
-                i = tmp - 1;
+                i++;
+
+            } else {
+                count = 0;
             }
         }
 
-        System.out.println(cnt);
-    }
-
-    static boolean check(int p, String s, String ioi) {
-        int size = p + (2 * N);
-        int j = 0;
-
-        if(size >= M) return false;
-
-        for (int i = p; i <= size; i++) {
-            if(s.charAt(i) != ioi.charAt(j++))
-                return false;
-        }
-
-        return true;
+        System.out.println(result);
     }
 }
