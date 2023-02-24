eb946ef9bc4515686c9107c2e8f3ca371da0e3ae
간호성 <ghs93@kakao.com>
2023-02-24 17:49:13 +0900

    base  code

---
+++ src/baekjoon/base/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.base;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 0000. title - rank 1
+ * @author hoseong
+ * @category
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
