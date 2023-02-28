22229cc9f58a933b5c29763fff1d863b5624ec4a
간호성 <ghs93@kakao.com>
2023-03-01 01:04:37 +0900

    SWEA 1230. 암호문3

--- src/swea/P1230/Solution.java
+++ src/swea/P1230/Solution.java
@@ -2,7 +2,6 @@ package swea.P1230;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -43,12 +42,22 @@ public class Solution {
                         insert();
                         break;
                     case 'D':
+                        delete();
                         break;
                     case 'A':
+                        add();
                         break;
                 }
             }
+
+            sb.append('#').append(test_case).append(' ');
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i)).append(' ');
+            }
+            sb.append('\n');
         }
+
+        System.out.println(sb);
     }
 
     static void insert() {
@@ -57,6 +66,23 @@ public class Solution {
 
         for (int i = x, cnt = x+y; i < cnt; i++) {
             list.add(i, Integer.parseInt(st.nextToken()));
+        }
+    }
+
+    static void delete() {
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < y; i++) {
+            list.remove(x);
+        }
+    }
+
+    static void add() {
+        int y = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < y; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
         }
     }
 }
