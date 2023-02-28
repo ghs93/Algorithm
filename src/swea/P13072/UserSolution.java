484211a87ff2879e7d780d8fe012a17176191062
간호성 <ghs93@kakao.com>
2023-03-01 01:05:01 +0900

    SWEA 13072. 병사관리

---
+++ src/swea/P13072/UserSolution.java
@@ -0,0 +1,37 @@
+package swea.P13072;
+
+/**
+ * SWEA 13072. 병사관리
+ * @author hoseong
+ * @category
+ */
+class UserSolution
+{
+    class Team {
+
+    }
+    public void init()
+    {
+    }
+
+    public void hire(int mID, int mTeam, int mScore)
+    {
+    }
+
+    public void fire(int mID)
+    {
+    }
+
+    public void updateSoldier(int mID, int mScore)
+    {
+    }
+
+    public void updateTeam(int mTeam, int mChangeScore)
+    {
+    }
+
+    public int bestSoldier(int mTeam)
+    {
+        return 0;
+    }
+}
