972982894c6a320c8b9224ce9feadf04a8f266d7
간호성 <ghs93@kakao.com>
2023-03-24 00:18:01 +0900

    14613. 리스트 복사

---
+++ src/swea/P14613/UserSolution.java
@@ -0,0 +1,89 @@
+package swea.P14613;
+
+import java.util.List;
+
+class UserSolution
+{
+    public class CustomList {
+        int idx;
+        int parent;
+        boolean isCopy;
+
+        CustomList(int idx, int parent, boolean isCopy) {
+            this.idx = idx;
+            this.parent = parent;
+            this.isCopy = isCopy;
+        }
+    }
+
+
+
+    /**
+     * 각 테스트 케이스의 처음에 호출된다.
+     * 테스트 케이스의 시작 시 생성되어 있는 리스트는 없다.
+     */
+    public void init()
+    {
+    }
+
+    /**
+     * mName 리스트가 생성되어 있지 않음이 보장된다.
+     * mName 리스트를 새로 생성한다.
+     * mName 리스트의 원소 개수는 mLength개가 된다.
+     * mName 리스트의 i번째 원소의 값은 mListValue[i]가 된다. ( 0 ≤ i ≤ mLength - 1 )
+     * 수행해야할 작업을 본문의 예시와 같이 나타내면, 아래와 같다.
+     * > mName = [mListValue[0], mListValue[1], …, mListValue[mLength - 1]]
+     * @param mName 리스트의 이름 ( 1 ≤ 길이 ≤ 20 )
+     * @param mLength 리스트의 길이 ( 1 ≤ mLength ≤ 200,000 )
+     * @param mListValue 원소들의 값 ( 0 ≤ mListValue[i] ≤ 32,767 )
+     */
+    public void makeList(char mName[], int mLength, int mListValue[])
+    {
+    }
+
+    /**
+     * mDest 리스트가 생성되어 있지 않음이 보장된다.
+     * mSrc 리스트가 생성되어 있음이 보장된다.
+     * mDest 리스트를 새로 생성한다.
+     * mSrc 리스트를 mDest 리스트에 복사한다.
+     *
+     * mCopy가 true인 경우, 값을 모두 복사하는 방식을 사용한다.
+     * 이때 수행해야할 작업을 본문의 예시와 같이 나타내면, 아래와 같다.
+     * > mDest = mSrc.copy()
+     *
+     * mCopy가 false인 경우, 주소만 복사하는 방식을 사용한다.
+     * 이때 수행해야할 작업을 본문의 예시와 같이 나타내면, 아래와 같다.
+     * > mDest = mSrc
+     * @param mDest 리스트의 이름 ( 1 ≤ 길이 ≤ 20 )
+     * @param mSrc 리스트의 이름 ( 1 ≤ 길이 ≤ 20 )
+     * @param mCopy 복사 방식 ( mCopy = true or false )
+     */
+    public void copyList(char mDest[], char mSrc[], boolean mCopy)
+    {
+    }
+
+    /**
+     * mName 리스트의 mIndex번째 원소의 값을 mValue로 변경한다.
+     * 수행해야할 작업을 본문의 예시와 같이 나타내면, 아래와 같다.
+     * > mName[mIndex] = mValue
+     * mName 리스트가 생성되어 있음이 보장된다.
+     * @param mName 리스트의 이름 ( 1 ≤ 길이 ≤ 20 )
+     * @param mIndex 원소의 인덱스 ( 0 ≤ mIndex ≤ mName 리스트의 길이 - 1 )
+     * @param mValue 원소의 수정 후의 값 ( 0 ≤ mValue ≤ 32,767 )
+     */
+    public void updateElement(char mName[], int mIndex, int mValue)
+    {
+    }
+
+    /**
+     * mName 리스트의 mIndex번째 원소를 반환한다.
+     * mName 리스트가 생성되어 있음이 보장된다.
+     * @param mName 리스트의 이름 ( 1 ≤ 길이 ≤ 20 )
+     * @param mIndex 원소의 인덱스 ( 0 ≤ mIndex ≤ mName 리스트의 길이 - 1 )
+     * @return mIndex번째 원소
+     */
+    public int element(char mName[], int mIndex)
+    {
+        return 0;
+    }
+}
