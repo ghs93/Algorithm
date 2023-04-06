d0343080b1d51940f7b26982e8de60d0294727c0
간호성 <ghs93@kakao.com>
2023-04-07 01:00:01 +0900

    CodeTree. 꼬리잡기놀이

---
+++ src/CodeTree/samsung_22_first_pm_1/Main.java
@@ -0,0 +1,18 @@
+package CodeTree.samsung_22_first_pm_1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * CodeTree. 꼬리잡기놀이
+ * @author hoseong
+ * @category Simulation, BFS
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+
+}
